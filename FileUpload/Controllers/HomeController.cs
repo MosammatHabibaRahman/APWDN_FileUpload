319e66828e805cfc162c1f102963a8a93b52c90c
MosammatHabibaRahman <habiba9g@yahoo.com>
2020-11-20 15:53:14 +0600

    File Upload Demo

---
+++ FileUpload/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using FileUpload.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FileUpload.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(ImageModel img)
+        {
+            string Filename = Path.GetFileNameWithoutExtension(img.ImageFile.FileName);
+            string FileExt = Path.GetExtension(img.ImageFile.FileName);
+            Filename = DateTime.Now.ToString("yyyyMMdd") + "-"+Filename.Trim()+ FileExt;
+            string UploadPath = ConfigurationManager.AppSettings["UserImagePath"].ToString();
+            img.ImagePath = UploadPath + Filename;
+            img.ImageFile.SaveAs(img.ImagePath);
+            return Content("Upload Successful!");
+        }
+    }
+}
---
+++ FileUpload/Models/ImageModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FileUpload.Models
+{
+    public class ImageModel
+    {
+        public string ImagePath { get; set; }
+        public HttpPostedFileBase ImageFile { get; set; }
+
+    }
+}
